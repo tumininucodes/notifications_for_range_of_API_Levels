2c0eba5d391a34c47d9dee0b8d8782b09cda6209
Tumi <oluwatumininuojo25@gmail.com>
2020-12-11 01:23:47 +0100

    Building custom layout for notifications in notification panel across a range or api levels to support compatibility

---
+++ app/src/main/java/com/kith/notificationexample/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.kith.notificationexample
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RemoteViews
+import androidx.databinding.DataBindingUtil
+import com.kith.notificationexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
+    private val channelId = "com.kith.notificationexample"
+    private val description = "Test notification"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        binding.btnNotify.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val contentView = RemoteViews(packageName, R.layout.notification_layout)
+            contentView.setTextViewText(R.id.tv_title, "Notification Example")
+            contentView.setTextViewText(R.id.tv_content, "Test notification")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel =
+                    NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this, channelId)
+                    .setContentTitle("Notification Example")
+                    .setContentText("Test Notification")
+                    .setSmallIcon(R.drawable.ic_android_black_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_android_black_24dp))
+                    .setContentIntent(pendingIntent)
+            } else {
+                builder = Notification.Builder(this)
+                    .setContentTitle("Notification Example")
+                    .setContentText("Test Notification")
+                    .setSmallIcon(R.drawable.ic_android_black_24dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_android_black_24dp))
+                    .setContentIntent(pendingIntent)
+            }
+            notificationManager.notify(1234, builder.build())
+        }
+    }
+}
